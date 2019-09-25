70484156a60c7d044abb693343bc1187ec6b511d
Castle46-white <cist.b217.y.shiro@gmail.com>
2019-09-25 11:05:39 +0900

    first commit

---
+++ src/main/java/ICTProject/CistPortal/CistPortalApplication.java
@@ -0,0 +1,13 @@
+package ICTProject.CistPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CistPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CistPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ICTProject/CistPortal/CistPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package ICTProject.CistPortal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CistPortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
