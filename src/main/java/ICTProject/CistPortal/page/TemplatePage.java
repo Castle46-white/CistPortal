98a446827e46ae953cdf2c1b7e06d92234ccbc26
Castle46-white <cist.b217.y.shiro@gmail.com>
2019-11-06 10:38:05 +0900

    TemplatePageの変更

--- src/main/java/ICTProject/CistPortal/page/TemplatePage.java
+++ src/main/java/ICTProject/CistPortal/page/TemplatePage.java
@@ -4,9 +4,8 @@ import ICTProject.CistPortal.page.panel.FooterPanel;
 import ICTProject.CistPortal.page.panel.HeaderPanel;
 import ICTProject.CistPortal.page.panel.StudentMenuPanel;
 import org.apache.wicket.markup.html.WebPage;
-import org.wicketstuff.annotation.mount.MountPath;
 
-@MountPath("TemplatePage")
+
 public class TemplatePage extends WebPage {
 
     public TemplatePage() {
