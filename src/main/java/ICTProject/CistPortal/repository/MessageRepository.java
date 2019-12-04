084535e9cf90e48e1b012360af39071650f62a14
cistb217 <cist.b217.y.yamauchi@gmail.com>
2019-12-04 11:29:13 +0900

    とりあえず動作確認完了

--- src/main/java/ICTProject/CistPortal/repository/MessageRepository.java
+++ src/main/java/ICTProject/CistPortal/repository/MessageRepository.java
@@ -21,14 +21,14 @@ public class MessageRepository implements IMessageRepository {
 
     @Override
     public int insertMessageInfo(String title, String contents, Timestamp deadline,Timestamp updateDate,String userId) {
-        String sql = "BEGIN insert into message (title, contents, deadline, update_date, user_id) "
+        String sql = " insert into message (title, contents, deadline, update_date, user_id) "
                 + "values (?,?,?,?,?) ";
         return jdbc.update(sql,title,contents,deadline,updateDate,userId);
     }
 
     @Override
     public long selectMessageId() {
-        String sql = "select max(id) from message commit ";
+        String sql = "select max(id) from message; ";
         var id = jdbc.query(sql,new SingleColumnRowMapper<>(Long.class),new Object[]{});
         return id.get(0);
     }
