1f955d6fd3799f072525baff7ae1f4aa3df93881
unknown <cist.b217.y.yamauchi@gmail.com>
2020-01-09 14:54:23 +0900

    pomの編集

--- src/main/java/ICTProject/CistPortal/CistPortalApplication.java
+++ src/main/java/ICTProject/CistPortal/CistPortalApplication.java
@@ -6,7 +6,9 @@ import org.apache.wicket.Page;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class CistPortalApplication extends WicketBootSecuredWebApplication {
 
